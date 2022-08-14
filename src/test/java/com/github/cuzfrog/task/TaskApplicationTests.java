bf7eb1b096f1f10fbef8f8f8ca6ac07c928791cb
Cause Chung <cuzfrog@139.com>
2022-08-14 16:18:59 +0800

    Add assertJ

--- src/test/java/com/github/cuzfrog/task/TaskApplicationTests.java
+++ src/test/java/com/github/cuzfrog/task/TaskApplicationTests.java
@@ -4,12 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 @ActiveProfiles("h2")
 class TaskApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThat(1).isEqualTo(1);
 	}
 
 }
